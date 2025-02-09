6f5ccf910982c08da0e7b0819041d8dbaefa8e83
Merey Kaliyev <yeahmerey@mail.ru>
2025-02-09 21:42:14 +0500

    new : {to-do-list is created}l

---
+++ LAB3/to-do-list/script.js
@@ -0,0 +1,31 @@
+const inputBox = document.getElementById('new-task');
+const taskList = document.getElementById('task-list');
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Please firstly write"); 
+    } else {
+        let li = document.createElement("li");
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("task-checkbox");
+        checkbox.addEventListener("change", function() {
+            li.classList.toggle("checked", checkbox.checked);
+        });
+                    
+        let taskText = document.createElement("span");
+        taskText.textContent = inputBox.value;
+
+        let deleteBtn = document.createElement("img");
+        deleteBtn.src = "img/delete.png";
+        deleteBtn.classList.add("delete-box");
+        deleteBtn.onclick = function() {
+            li.remove();
+        };                    
+        li.appendChild(checkbox);
+        li.appendChild(taskText);
+        li.appendChild(deleteBtn);
+        taskList.appendChild(li);
+    }
+    inputBox.value = ''; 
+}
