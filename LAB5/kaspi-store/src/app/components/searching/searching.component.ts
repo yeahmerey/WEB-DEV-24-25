e50bb7667a2377db4a67ca350a90518440879802
Merey Kaliyev <yeahmerey@mail.ru>
2025-02-26 16:04:05 +0500

    task from ass { doing searching logic ,, ahhahhahah , nice:) }

--- LAB5/kaspi-store/src/app/components/searching/searching.component.ts
+++ LAB5/kaspi-store/src/app/components/searching/searching.component.ts
@@ -27,4 +27,19 @@ export class SearchingComponent {
     this.selectedCategory = category ;
     this.filteredList = category ? this.productList.filter(p => p.category === category): [...this.productList]; 
   }
+  filterBySearch(searchText: string) {
+    if (!searchText) {
+      this.filteredList = this.selectedCategory  ? this.productList.filter(p => p.category === this.selectedCategory) : [...this.productList];
+      return;
+    }
+    const searchLower = searchText.toLowerCase();
+    this.filteredList = this.productList.filter(product => {
+      const matchesCategory = !this.selectedCategory || product.category === this.selectedCategory;
+      const matchesSearch = product.name.toLowerCase().includes(searchLower) || 
+                           product.description?.toLowerCase().includes(searchLower);
+      
+      return matchesCategory && matchesSearch;
+    });
+  }
+
 }
