0edd6533484cee3aa14caa08cb4c71d7729af1a1
Merey Kaliyev <yeahmerey@mail.ru>
2025-03-13 18:23:00 +0000

    fix : [idk : some changing]

--- LAB8/shopBack/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from api.models import Product 
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id" , "name" , "price" , "description")
-    search_fields = ("name" , "price")
--- LAB8/shopBack/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path , re_path
-from api import views
-urlpatterns = [
-    path('hello/', views.hello_world),
-    path('about/' , views.about),
-    re_path(r'^time/plus/(\d+)/$', views.hour_ahead),
-    path("test-json/", views.test_json), 
-    path('products/', views.products_list)
-]   
