4836369c3250756918108285cb3e3d9dc7f44ca0
Merey Kaliyev <yeahmerey@mail.ru>
2025-04-09 15:37:07 +0500

    fix:[companies/vacancies endpoint is fixed in front]

--- lab_10/api/serializers.py
+++ lab_10/api/serializers.py
@@ -9,17 +9,6 @@ class CompanySerializer(serializers.Serializer):
     city = serializers.CharField(max_length=255)
     address = serializers.CharField(max_length=255)
 
-    def create(self, validated_data):
-        instance = Company(name = validated_data['name'], description = validated_data['description'], city = validated_data['city'], address = validated_data['address'])
-        instance.save()
-        return instance
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.city = validated_data.get('city', instance.city)
-        instance.address = validated_data.get('address', instance.address)
-        instance.save()
     
 
 class VacancySerializer(serializers.ModelSerializer):
