6fdc45235c9242bdc56371f44d54e6859395b978
Merey Kaliyev <yeahmerey@mail.ru>
2025-04-09 08:17:32 +0500

    feat:[cbv,fbv,generics are added]

--- lab_10/hhBack/api/urls.py
+++ lab_10/hhBack/api/urls.py
@@ -1,12 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path
-from api import views , views_w_ser , views_w_m_ser
+#from api import views , views_w_ser , views_w_m_ser
+from api.views import  CompanyListAPIView , CompanyDetailAPIView
+
 urlpatterns = [
-    path('companies/', views_w_m_ser.companies_list), 
-    path('companies/<int:company_id>/' , views_w_m_ser.company_details), 
-    path('companies/<int:company_id>/vacancies/' , views_w_m_ser.company_vacancies), 
-    path('vacancies/' , views_w_m_ser.vacancies_list), 
-    path('vacancies/<int:vacancy_id>' , views_w_m_ser.vacancy_details), 
-    path('vacancies/top10/' , views_w_m_ser.topTen_vacancies), 
+    path('companies/', CompanyListAPIView.as_view()), 
+    path('companies/<int:company_id>/' , CompanyDetailAPIView.as_view()), 
+    #path('companies/<int:company_id>/vacancies/' , views_w_m_ser.company_vacancies), 
+    #path('vacancies/' , views_w_m_ser.vacancies_list), 
+    #path('vacancies/<int:vacancy_id>' , views_w_m_ser.vacancy_details), 
+    #path('vacancies/top10/' , views_w_m_ser.topTen_vacancies), 
 ]
---
+++ lab_10/hhBack/api/views/__init__.py
@@ -0,0 +1,4 @@
+#from .fbv import companies_list , company_detail
+#from .cbv import CompanyDetailAPIView , CompanyListAPIView
+#from .generic_v1 import CompanyListAPIView , CompanyDetailAPIView
+from .generic_v2 import CompanyListAPIView , CompanyDetailAPIView
